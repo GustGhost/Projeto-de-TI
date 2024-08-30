2c5a7d82cc84f757d64e1ec6ecdda1e33094aae3
AnaQueiroz1 <anaqrocha11@gmail.com>
2024-08-30 09:24:22 -0300

    atualização

--- src/test/app_test.py
+++ src/test/app_test.py
@@ -1,7 +1,12 @@
     # Primeiramente o cliente manda uma mensagem para o número de Telefone
     
     #Exemplo: "Olá Estou vindo do site e gostaria de participar de um desafio"
- 
+'''
+ problemas para resolver: loop do nome que é para ficar só o inicial e
+ quando escolho uma opção, exemplo a 2, ao final de toda mensagem aparece a primeira. 
+ essa daqui: vou mandar print para vocÊ no wpp.
+ "Entendido! Vamos te passar mais informações."
+ '''
 def responder_mensagem(mensagem_recebida):
     # Primeira resposta automática
     resposta_inicial = "SUPERE-SE desafios agradece seu contato. Como podemos ajudar?"
@@ -36,13 +41,38 @@ def responder_mensagem(mensagem_recebida):
     # Responde de acordo com a opção escolhida
     if opcao_escolhida == "1":
         resposta_opcao = "Ótimo! Vamos prosseguir com a inscrição."
+        #perguntar sobre o envio das informações
+        print(f'Resposta enviada {resposta_opcao}')
+        print("Pergunta enviada: Posso te enviar as informações de inscrição agora? (Sim/Não)")
+        resposta_inscricao = input("Cliente responde: ")
+        if resposta_inscricao.lower() == "sim":
+            print("Resposta enviada: Excelente! As informações de inscrição estão a caminho.")
+            #informações adicionais
+        else:
+            print("Resposta enviada: Sem problemas, podemos falar em outro momento.")
+            # Encerrar ou continuar conforme o fluxo
+            
+            
     elif opcao_escolhida == "2":
         resposta_opcao = "Entendido! Vamos te passar mais informações."
+        print(f"Resposta enviada: {resposta_opcao}")
+        # Perguntar qual informação deseja mais detalhes
+        print("Pergunta enviada: Sobre o que gostaria de saber mais? (Desafios, Recompensas, Regras)")
+        resposta_detalhes = input("Cliente responde: ")
+        if resposta_detalhes.lower() in ["desafios", "recompensas", "regras"]:
+            print(f"Resposta enviada: Enviando mais informações sobre {resposta_detalhes}.")
+            # Fornecer mais informações conforme a escolha
+        else:
+            print("Resposta enviada: Não entendi, poderia repetir?")
+            # Repetir ou encerrar
+        
     elif opcao_escolhida == "3":
         resposta_opcao = "Que bom que quer conhecer! Vamos te explicar tudo."
+        print(f"Resposta enviada:{resposta_opcao}")
+        #apresentação geral dos desafios por áudio
+        print("Resposta enviada: Vamos te apresentar um resumo sobre nossos desafios...")
     else:
         resposta_opcao = "Opção inválida. Por favor, escolha uma das opções disponíveis."
-
     print(f"Resposta enviada: {resposta_opcao}")
 
 
